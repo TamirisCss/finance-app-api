e9d5ba5f720a243f8b7ab00587d98e9f6dffe11f
Tamiris Cristine <tamiris.css@gmail.com>
2023-11-23 11:46:26 +0100

    feat: add CreateUserCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        //TODO: VERIFY IF EMAIL ALREADY EXISTS
+
+        //GENARATE USER ID
+        const userId = uuidv4()
+
+        //CRYPT PASSWORD
+        const passwordHash = await bcrypt.hash(createUserParams.password, 10)
+
+        //INSERT UDER ON DB (POSTGRES)
+        const user = {
+            ...createUserParams,
+            ID: userId,
+            password: passwordHash,
+        }
+
+        //CALL REPOSITORY
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createUser = await postgresCreateUserRepository.execute(user)
+
+        return createUser
+    }
+}
