d777d16704e618b606bd2919a0152f501df31ada
Tamiris Cristine <tamiris.css@gmail.com>
2023-11-29 17:03:43 +0100

    feat: add updateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        //1- if email is being  updated, verify if email already exists
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+        const user = {
+            ...updateUserParams,
+        }
+
+        //2- if password is being updated, crypt password
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+        //2- call repository to update user
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}
