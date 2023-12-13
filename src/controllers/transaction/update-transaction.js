3e73720fca4d4a5547840d8994cd29911a90eaca
Tamiris Cristine <tamiris.css@gmail.com>
2023-12-13 15:09:23 +0100

    feat: add UpdateTransactionController

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,57 @@
+import {
+    badRequest,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+} from '../helpers/index.js'
+
+export class UpdateTransactionController {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const isIdValid = checkIfIdIsValid(httpRequest.params.transactionId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            const allowedFields = ['name', 'date', 'amount', 'type']
+
+            const someFieldIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            )
+
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed.',
+                })
+            }
+
+            if (params.amount) {
+                const typeIsValid = checkIfTypeIsValid(params.type)
+
+                if (!typeIsValid) {
+                    return invalidIdResponse()
+                }
+            }
+
+            const transaction = await this.updateTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+                params,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            console.error(error)
+
+            return serverError()
+        }
+    }
+}
