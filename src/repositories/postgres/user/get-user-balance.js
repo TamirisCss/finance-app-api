237aafcdcee4c684687c3fa76fc476d67ad16a92
Tamiris Cristine <tamiris.css@gmail.com>
2024-03-25 11:47:18 +0100

    feat: ensure GetUserBalanceRepository use Prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,25 +1,57 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { Prisma } from '@prisma/client'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const balance = await PostgresHelper.query(
-            `SELECT
-                SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-                SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-                SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-                (
-                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-                ) AS balance
-            FROM transactions
-            WHERE user_id = $1;`,
-            [userId],
+        const {
+            _sum: { amount: totalExpenses },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalEarnings },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalInvestments },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
+        const _totalExpenses = totalExpenses || new Prisma.Decimal(0)
+        const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
+
+        const balance = new Prisma.Decimal(
+            _totalEarnings - _totalExpenses - _totalInvestments,
         )
 
         return {
-            userId,
-            ...balance[0],
+            earnings: _totalEarnings,
+            expenses: _totalExpenses,
+            investments: _totalInvestments,
+            balance,
         }
     }
 }
