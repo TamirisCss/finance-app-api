7a6f66843183cb7c7c436f4173b2d680706c5440
Tamiris Cristine <tamiris.css@gmail.com>
2024-04-10 10:57:09 +0200

    test: ensure DeleteTransactionController returns 500 when DeleteTransactionUseCase throws

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -41,4 +41,30 @@ describe('DeleteTransactionController', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    // it('should return 404 when transaction is not found', async () => {
+    //     const { sut, deleteTransactionUseCase } = makeSut()
+
+    //     jest.spyOn(deleteTransactionUseCase, 'execute').mockResolvedValue(null)
+
+    //     const result = await sut.execute({
+    //         params: { transactionId: faker.string.uuid() },
+    //     })
+
+    //     expect(result.statusCode).toBe(404)
+    // })
+
+    it('should return 500 when use case throws', async () => {
+        const { sut, deleteTransactionUseCase } = makeSut()
+
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const result = await sut.execute({
+            params: { transactionId: faker.string.uuid() },
+        })
+
+        expect(result.statusCode).toBe(500)
+    })
 })
