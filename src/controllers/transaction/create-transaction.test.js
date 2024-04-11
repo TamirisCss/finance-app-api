8fb331a2b34e1afa68b714899e4b7f157f0afcf2
Tamiris Cristine <tamiris.css@gmail.com>
2024-04-11 15:26:25 +0200

    test: ensure CreateTransactionController calls CreateTransactionUseCase with correct params

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -178,4 +178,14 @@ describe('Create Transaction Controller', () => {
 
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call CreateTransactionUseCase with correct params', async () => {
+        const { sut, createTransactionUseCase } = makeSut()
+
+        const executeSpy = jest.spyOn(createTransactionUseCase, 'execute')
+
+        await sut.execute(baseHttpRequest)
+
+        expect(executeSpy).toHaveBeenCalledWith(baseHttpRequest.body)
+    })
 })
