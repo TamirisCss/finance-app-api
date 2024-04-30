9b8b222c2e25a78271737469e729e6a279afbf1c
Tamiris Cristine <tamiris.css@gmail.com>
2024-04-30 15:57:03 +0200

    test: ensure GetUserByIdUseCase throws if GetUserByIdRepository throws

--- src/use-cases/user/get-user-by-id.test.js
+++ src/use-cases/user/get-user-by-id.test.js
@@ -46,4 +46,15 @@ describe('GetUserByIdUseCase', () => {
 
         expect(executeSpy).toHaveBeenCalledWith(userId)
     })
+
+    it('should throw if GetUserByIdRepository throws', async () => {
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        const promise = sut.execute(faker.string.uuid())
+
+        await expect(promise).rejects.toThrow()
+    })
 })
