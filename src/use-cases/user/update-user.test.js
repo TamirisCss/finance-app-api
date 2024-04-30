da776649ab9370c5d28a038c546ea31739e8a600
Tamiris Cristine <tamiris.css@gmail.com>
2024-04-30 16:09:05 +0200

    test: ensure UpdateUserUseCase updates user successfully (with email)

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -59,4 +59,20 @@ describe('UpdateUserUseCase', () => {
 
         expect(result).toBe(user)
     })
+
+    it('should update user successfully (with email)', async () => {
+        const { sut, getUserByEmailRepository } = makeSut()
+        const getUserByEmailRepositorySpy = jest.spyOn(
+            getUserByEmailRepository,
+            'execute',
+        )
+        const email = faker.internet.email()
+
+        const result = await sut.execute(faker.string.uuid(), {
+            email,
+        })
+
+        expect(getUserByEmailRepositorySpy).toHaveBeenCalledWith(email)
+        expect(result).toBe(user)
+    })
 })
