dc3737f5aac6a499545e87180bfcf37475dc58ae
Tamiris Cristine <tamiris.css@gmail.com>
2024-05-02 14:32:59 +0200

    test: ensure CreateTransactionUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -124,4 +124,15 @@ describe('CreateTransactionUseCase', () => {
 
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        const { sut, idGeneratorAdapter } = makeSut()
+        jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const promise = sut.execute(createTransactionParams)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
