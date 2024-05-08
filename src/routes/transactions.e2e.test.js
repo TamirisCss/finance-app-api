6fcbe905cfac6e2014345d8060b1b5bc0e174302
Tamiris Cristine <tamiris.css@gmail.com>
2024-05-08 12:04:35 +0200

    test: ensure GET /api/transaction returns 404 when fetching transactions from a non-existing user

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -82,4 +82,28 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.status).toBe(200)
         expect(response.body.id).toBe(createdTransaction.id)
     })
+
+    // it('PATCH /api/transactions/:transactionId should return 404 when updating a non-existing transaction', async () => {
+    //     const response = await request(app)
+    //         .patch(`/api/transactions/${transaction.id}`)
+    //         .send({ amount: 100, type: TransactionType.INVESTMENT })
+
+    //     expect(response.status).toBe(404)
+    // })
+
+    // it('DELETE /api/transactions/:transactionId should return 404 when deleting a non-existing transaction', async () => {
+    //     const response = await request(app).delete(
+    //         `/api/transactions/${transaction.id}`
+    //     )
+
+    //     expect(response.status).toBe(404)
+    // })
+
+    it('GET /api/transaction?userId should return 404 when fetching transactions from a non-existing user', async () => {
+        const response = await request(app).get(
+            `/api/transactions?userId=${transaction.user_id}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
 })
