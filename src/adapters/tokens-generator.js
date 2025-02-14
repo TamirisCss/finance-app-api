3c29b28bde30d3f6f3e20ace6a6ec42f10613fda
Tamiris Cristine <tamiris.css@gmail.com>
2025-02-14 14:44:58 +0100

    Fix: update tokens name

--- src/adapters/tokens-generator.js
+++ src/adapters/tokens-generator.js
@@ -3,12 +3,20 @@ import jwt from 'jsonwebtoken'
 export class TokensGeneratorAdapter {
     execute(userId) {
         return {
-            accessToken: jwt.sign({ userId }, process.env.JWT_SECRET, {
-                expiresIn: '15min',
-            }),
-            refreshToken: jwt.sign({ userId }, process.env.JWT_SECRET, {
-                expiresIn: '30d',
-            }),
+            accessToken: jwt.sign(
+                { userId },
+                process.env.JWT_ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: '15min',
+                },
+            ),
+            refreshToken: jwt.sign(
+                { userId },
+                process.env.JWT_REFRESH_TOKEN_SECRET,
+                {
+                    expiresIn: '30d',
+                },
+            ),
         }
     }
 }
